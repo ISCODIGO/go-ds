f62213ae5febec44b320174f7dfa72246e7c08e1
Enrique Avila <senquevila@gmail.com>
2024-11-19 13:44:49 -0600

    public functions

--- stack/arraystack.go
+++ stack/arraystack.go
@@ -7,9 +7,9 @@ var ErrStackUnderflow = errors.New("pila vacia")
 var ErrStackOverflow = errors.New("pila llena")
 
 type ArrayStack struct {
-	top      int  // posicion de la cima
-	size     int  // tamaño de la pila
-	capacity int  // capacidad del slice
+	top      int   // posicion de la cima
+	size     int   // tamaño de la pila
+	capacity int   // capacidad del slice
 	data     []int // slice
 }
 
@@ -22,11 +22,11 @@ func NewArrayStack(capacidad int) ArrayStack {
 	}
 }
 
-func (pila ArrayStack) isEmpty() bool {
-	return pila.size == 0
+func (pila ArrayStack) IsEmpty() bool {
+	return pila.top == -1
 }
 
-func (pila ArrayStack) isFull() bool {
+func (pila ArrayStack) IsFull() bool {
 	return pila.top == pila.capacity-1
 }
 
@@ -36,9 +36,9 @@ func (pila *ArrayStack) Clear() {
 }
 
 func (pila *ArrayStack) Push(e int) (err error) {
-	if pila.isFull() {
+	if pila.IsFull() {
 		err = ErrStackOverflow
-		return  // devuelve la variable err
+		return // devuelve la variable err
 	}
 
 	pila.top++
@@ -49,13 +49,13 @@ func (pila *ArrayStack) Push(e int) (err error) {
 }
 
 func (pila ArrayStack) Top() (e int, err error) {
-	if pila.isEmpty() {
+	if pila.IsEmpty() {
 		err = ErrStackUnderflow
 		return // devuelve e y err
 	}
 
 	e = pila.data[pila.top]
-	return  // devuelve e y err
+	return // devuelve e y err
 }
 
 func (pila *ArrayStack) Pop() (e int, err error) {
@@ -69,7 +69,6 @@ func (pila *ArrayStack) Pop() (e int, err error) {
 	pila.size--
 	return
 }
-
 
 func (pila ArrayStack) Size() int {
 	return pila.size
